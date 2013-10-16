daf75379fccbd0df940c5c7e942fd4a9cf45bd10
Kevin Lozandier <lozandier@gmail.com>
2013-10-16 01:44:33 -0700

    Correctly readded assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,7 @@ Personal::Application.configure do
   config.assets.initialize_on_precompile = false 
 
   # Generate digests for assets URLs.
-  config.assets.digest = true #temporarily
+  config.assets.digest = true 
 
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
