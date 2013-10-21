68d388dc813e790f7bb1368efcb54dd133545945
Chad Pytel <cpytel@thoughtbot.com>
2013-10-21 10:48:40 -0400

    remove precompiled assets

--- public/assets/reenable_turbolinks-348cd1157e057c9bb72419affb12d088.js
+++
@@ -1,4 +0,0 @@
-(function() {
-  $("a, .personas.index a, .personas.show a, .projects.index a, .projects.show a, .top-bar a[data-no-turbolink], [data-no-turbolink]").removeData('noTurbolink').removeAttr('data-no-turbolink').attr('data-reenabled-turbolinks', 'true');
-
-}).call(this);
